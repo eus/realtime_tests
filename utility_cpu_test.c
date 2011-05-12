ca84f64e21b40b2b2b6779f1d8b95ae31d5ef826
Tadeus Prastowo <eus@member.fsf.org>
2011-05-12 12:23:40 +0200

    [infrastructure testcase] utility_cpu: Remove unused _GNU_SOURCE.

--- utility_cpu_test.c
+++ utility_cpu_test.c
@@ -15,8 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
  *****************************************************************************/
 
-#define _GNU_SOURCE
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
