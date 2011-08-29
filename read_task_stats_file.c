6bfcbdffa911cf59862d46011935200c580ea8a9
Tadeus Prastowo <eus@member.fsf.org>
2011-08-30 00:27:03 +0200

    [Infrastructure Component] * read_task_stats_file.c: Fix a fatal mistake in reporting execution   time (t_finish - t_start) as the response time (t_finish -   t_release).

--- read_task_stats_file.c
+++ read_task_stats_file.c
@@ -25,25 +25,27 @@
 #include "utility_time.h"
 #include "task.h"
 
-struct exec_time_list
+struct response_time_list
 {
-  struct exec_time_list *next;
+  struct response_time_list *next;
   unsigned job_count;
-  unsigned long long exec_time;
+  unsigned long long response_time;
 };
 
-static void free_exec_time_list(struct exec_time_list *smallest_exec_time)
+static void
+free_response_time_list(struct response_time_list *smallest_response_time)
 {
-  while (smallest_exec_time != NULL) {
-    struct exec_time_list *n = smallest_exec_time;
-    smallest_exec_time = n->next;
+  while (smallest_response_time != NULL) {
+    struct response_time_list *n = smallest_response_time;
+    smallest_response_time = n->next;
     free(n);
   }
 }
 
-static struct exec_time_list *new_exec_time_list(unsigned long long exec_time)
+static struct response_time_list *
+new_response_time_list(unsigned long long response_time)
 {
-  struct exec_time_list *res = malloc(sizeof(*res));
+  struct response_time_list *res = malloc(sizeof(*res));
   if (res == NULL) {
     log_error("Insufficient memory to create exec time list item");
     return NULL;
@@ -51,31 +53,31 @@ static struct exec_time_list *new_exec_time_list(unsigned long long exec_time)
 
   res->next = NULL;
   res->job_count = 1;
-  res->exec_time = exec_time;
+  res->response_time = response_time;
 
   return res;
 }
 
-static struct exec_time_list *
-insert_exec_time(unsigned long long exec_time,
-                 struct exec_time_list *smallest_exec_time)
+static struct response_time_list *
+insert_response_time(unsigned long long response_time,
+                     struct response_time_list *smallest_response_time)
 {
-  /* Adjust exec_time unit to microsecond from nanosecond */
-  exec_time = (exec_time % 1000 >= 500
-               ? exec_time / 1000 + 1
-               : exec_time / 1000);
-  /* END: Adjust exec_time unit to microsecond from nanosecond */
+  /* Adjust response_time unit to microsecond from nanosecond */
+  response_time = (response_time % 1000 >= 500
+                   ? response_time / 1000 + 1
+                   : response_time / 1000);
+  /* END: Adjust response_time unit to microsecond from nanosecond */
 
-  struct exec_time_list *itr_prev = NULL;
-  struct exec_time_list *itr = smallest_exec_time;
+  struct response_time_list *itr_prev = NULL;
+  struct response_time_list *itr = smallest_response_time;
 
   while (itr != NULL) {
 
-    if (itr->exec_time == exec_time) {
+    if (itr->response_time == response_time) {
       /* The item already exists; just increment job_count and exit */
       itr->job_count++;
-      return smallest_exec_time;
-    } else if (itr->exec_time > exec_time) {
+      return smallest_response_time;
+    } else if (itr->response_time > response_time) {
       /* Found an insertion point */
       break;
     }
@@ -85,7 +87,7 @@ insert_exec_time(unsigned long long exec_time,
   }
 
   /* +----------+----------+------------------------------+
-   * | itr_prev | itr      | exec_time                    |
+   * | itr_prev | itr      | response_time                |
    * +----------+----------+------------------------------+
    * | NULL     | NULL     | smallest                     |
    * | NULL     | not NULL | smallest                     |
@@ -95,64 +97,67 @@ insert_exec_time(unsigned long long exec_time,
    */
   if (itr_prev == NULL && itr == NULL) {
 
-    smallest_exec_time = new_exec_time_list(exec_time);
+    smallest_response_time = new_response_time_list(response_time);
 
   } else if (itr_prev == NULL && itr != NULL) {
 
-    smallest_exec_time = new_exec_time_list(exec_time);
-    if (smallest_exec_time == NULL) {
+    smallest_response_time = new_response_time_list(response_time);
+    if (smallest_response_time == NULL) {
       return itr;
     }
 
-    smallest_exec_time->next = itr;
+    smallest_response_time->next = itr;
 
   } else if (itr_prev != NULL && itr == NULL) {
 
-    struct exec_time_list *largest_exec_time = new_exec_time_list(exec_time);
-    itr_prev->next = largest_exec_time;
+    struct response_time_list *largest_response_time;
+    largest_response_time = new_response_time_list(response_time);
+
+    itr_prev->next = largest_response_time;
 
   } else {
 
-    struct exec_time_list *n = new_exec_time_list(exec_time);
+    struct response_time_list *n = new_response_time_list(response_time);
     if (n == NULL) {
-      return smallest_exec_time;
+      return smallest_response_time;
     }
 
     n->next = itr;
     itr_prev->next = n;
   }
 
-  return smallest_exec_time;
+  return smallest_response_time;
 }
 
-static void print_gnuplot_cdf(struct exec_time_list *smallest_exec_time,
+static void print_gnuplot_cdf(struct response_time_list *smallest_response_time,
                               unsigned total_job_count, FILE *report)
 {
   unsigned cummulative_job_count = 0;
 
-  struct exec_time_list *itr = smallest_exec_time;
+  struct response_time_list *itr = smallest_response_time;
   while (itr != NULL) {
     cummulative_job_count += itr->job_count;
 
     fprintf(report, "%llu\t%.06f\n",
-            itr->exec_time, cummulative_job_count / (double) total_job_count);
+            itr->response_time,
+            cummulative_job_count / (double) total_job_count);
 
     itr = itr->next;
   }
 }
 
-static void print_matlab_cdf(struct exec_time_list *smallest_exec_time,
+static void print_matlab_cdf(struct response_time_list *smallest_response_time,
                              unsigned total_job_count, FILE *report)
 {
   fprintf(report, "plot([");
-  struct exec_time_list *itr = smallest_exec_time;
+  struct response_time_list *itr = smallest_response_time;
   while (itr != NULL) {
-    fprintf(report, " %llu", itr->exec_time);
+    fprintf(report, " %llu", itr->response_time);
 
     itr = itr->next;
   }
   fprintf(report, "], [");
-  itr = smallest_exec_time;
+  itr = smallest_response_time;
   unsigned cummulative_job_count = 0;
   while (itr != NULL) {
     cummulative_job_count += itr->job_count;
@@ -169,9 +174,10 @@ enum cdf_format {
   GNUPLOT,
   MATLAB,
 };
-static void print_exec_time_cdf(struct exec_time_list *smallest_exec_time,
-                                unsigned total_job_count, FILE *report,
-                                enum cdf_format cdf_fmt)
+static void
+print_response_time_cdf(struct response_time_list *smallest_response_time,
+                        unsigned total_job_count, FILE *report,
+                        enum cdf_format cdf_fmt)
 {
   if (total_job_count == 0) {
     return;
@@ -179,10 +185,10 @@ static void print_exec_time_cdf(struct exec_time_list *smallest_exec_time,
 
   switch(cdf_fmt) {
   case GNUPLOT:
-    print_gnuplot_cdf(smallest_exec_time, total_job_count, report);
+    print_gnuplot_cdf(smallest_response_time, total_job_count, report);
     break;
   case MATLAB:
-    print_matlab_cdf(smallest_exec_time, total_job_count, report);
+    print_matlab_cdf(smallest_response_time, total_job_count, report);
     break;
   case NO_CDF:
     return;
@@ -208,7 +214,7 @@ struct task_stats
   unsigned late_count;
   int suppress_printout;
 
-  struct exec_time_list *exec_times;
+  struct response_time_list *response_times;
 };
 
 static int print_task_stats(task *tau, void *args)
@@ -254,9 +260,9 @@ static int print_job_stats(job_statistics *stats, void *args)
 
   if (!prms->suppress_printout) {
     if (prms->nth_job == 1) {
-      fprintf(prms->report, "%5s%15s%15s%15s%15s%15s%15s%15s\n",
+      fprintf(prms->report, "%5s%15s%15s%15s%15s%15s%15s%15s%15s\n",
               "#job", "release", "delta_s", "start", "deadline", "delta_f",
-              "finish", "exec_time");
+              "finish", "exec_time", "response_time");
     }
 
     fprintf(prms->report, "%5d", prms->nth_job);
@@ -314,8 +320,7 @@ static int print_job_stats(job_statistics *stats, void *args)
   /* End of start time */
 
   /* Finishing time */
-  absolute_time *t_deadline = utility_time_add_dyn_gc(t_release,
-                                                      &prms->deadline);
+  absolute_time *t_deadline = utility_time_add_dyn(t_release, &prms->deadline);
   if (!prms->suppress_printout) {
     t_str = to_string_dyn(t_deadline);
     fprintf(prms->report, "%15s", t_str);
@@ -367,13 +372,9 @@ static int print_job_stats(job_statistics *stats, void *args)
   /* End of finishing time */
 
   /* Execution time */
-  relative_time *exec_time = utility_time_sub_dyn_gc(t_finish, t_start);
+  relative_time *exec_time = utility_time_sub_dyn(t_finish, t_start);
+  utility_time_gc(t_start);
 
-  struct timespec exec_time_duration;
-  to_timespec(exec_time, &exec_time_duration);
-  prms->exec_times = insert_exec_time(exec_time_duration.tv_sec * 1000000000ULL
-                                      + exec_time_duration.tv_nsec,
-                                      prms->exec_times);
   if (!prms->suppress_printout) {
     t_str = to_string_dyn_gc(exec_time);
     fprintf(prms->report, "%15s", t_str);
@@ -382,6 +383,25 @@ static int print_job_stats(job_statistics *stats, void *args)
     utility_time_gc(exec_time);
   }
   /* End of execution time */
+
+  /* Response time */
+  relative_time *response_time = utility_time_sub_dyn_gc(t_finish, t_release);
+
+  struct timespec response_time_duration;
+  to_timespec(response_time, &response_time_duration);
+  prms->response_times = insert_response_time((response_time_duration.tv_sec
+                                               * 1000000000ULL)
+                                              + response_time_duration.tv_nsec,
+                                              prms->response_times);
+
+  if (!prms->suppress_printout) {
+    t_str = to_string_dyn_gc(response_time);
+    fprintf(prms->report, "%15s", t_str);
+    free(t_str);
+  } else {
+    utility_time_gc(response_time);
+  }
+  /* End of response time */
 
   if (!prms->suppress_printout) {
     fprintf(prms->report, "%s\n", is_late ? " LATE" : "");
@@ -456,7 +476,7 @@ int main(int argc, char **argv, char **envp)
     .report = stdout,
     .nth_job = 1,
     .late_count = 0,
-    .exec_times = NULL,
+    .response_times = NULL,
     .suppress_printout = (cdf_fmt != NO_CDF),
   };
   utility_time_init(&stats_prms.period);
@@ -471,10 +491,10 @@ int main(int argc, char **argv, char **envp)
 
   utility_file_close(stats_file, argv[1]);
 
-  print_exec_time_cdf(stats_prms.exec_times, stats_prms.nth_job - 1,
-                      stats_prms.report, cdf_fmt);
+  print_response_time_cdf(stats_prms.response_times, stats_prms.nth_job - 1,
+                          stats_prms.report, cdf_fmt);
 
-  free_exec_time_list(stats_prms.exec_times);
+  free_response_time_list(stats_prms.response_times);
 
   return EXIT_SUCCESS;
 }
