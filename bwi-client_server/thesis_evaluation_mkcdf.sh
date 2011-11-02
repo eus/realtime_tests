9705115e567c334acb4cb0a4aa7a1210a607a475
Tadeus Prastowo <eus@member.fsf.org>
2011-11-02 13:38:29 +0100

    [Experimentation Component] * bwi-client_server/thesis_evaluation_mkcdf.sh: Change the line width   from 3 to 2. Only plot the points every 20 steps to make it look   nicer although losing some topography of the increasing slope (the   lost is okay because CDF is monotonically increasing anyway).

--- bwi-client_server/thesis_evaluation_mkcdf.sh
+++ bwi-client_server/thesis_evaluation_mkcdf.sh
@@ -8,7 +8,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lw 3 title "Soft replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lw 2 title "Soft replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_no_soft.eps";
 replot'
@@ -17,7 +17,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lw 3 title "Hard replenishment";
+plot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_no_hard.eps";
 replot'
@@ -27,8 +27,8 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lw 3 title "Soft replenishment";
-replot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lw 3 title "Hard replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lw 2 title "Soft replenishment";
+replot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_no.eps";
 replot'
@@ -40,7 +40,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lw 3 title "Soft replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lw 2 title "Soft replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_yes_soft.eps";
 replot'
@@ -49,7 +49,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lw 3 title "Hard replenishment";
+plot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_yes_hard.eps";
 replot'
@@ -59,8 +59,8 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lw 3 title "Soft replenishment";
-replot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lw 3 title "Hard replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lw 2 title "Soft replenishment";
+replot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_yes.eps";
 replot'
@@ -72,10 +72,10 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_no_hrt.txt" w steps lw 3 title "Soft replenishment in experiment without BWI";
-replot "thesis_evaluation_hard_'$i'_BWI_no_hrt.txt" w steps lw 3 title "Hard replenishment in experiment without BWI";
-replot "thesis_evaluation_soft_'$i'_BWI_yes_hrt.txt" w linespoints lt 1 pt 2 lw 1 title "Soft replenishment in experiment with BWI";
-replot "thesis_evaluation_hard_'$i'_BWI_yes_hrt.txt" w linespoints lt 1 pt 6 lw 1 title "Hard replenishment in experiment with BWI";
+plot "thesis_evaluation_soft_'$i'_BWI_no_hrt.txt" w steps lw 2 title "Soft replenishment in experiment without BWI";
+replot "thesis_evaluation_hard_'$i'_BWI_no_hrt.txt" w steps lw 2 title "Hard replenishment in experiment without BWI";
+replot "thesis_evaluation_soft_'$i'_BWI_yes_hrt.txt" every 20 w linespoints pt 2 lt 1 lw 2 title "Soft replenishment in experiment with BWI";
+replot "thesis_evaluation_hard_'$i'_BWI_yes_hrt.txt" every 20 w linespoints pt 6 lt 1 lw 2 title "Hard replenishment in experiment with BWI";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_hrt.eps";
 replot'
