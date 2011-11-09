72fc901bd5be68e8d1733a130f2b57c605d9c083
Tadeus Prastowo <eus@member.fsf.org>
2011-11-09 21:54:43 +0100

    [Experimentation Component] * bwi-client_server/thesis_evaluation_mkcdf.sh: Separate the CDF plots   of the HRT task to "with BWI" and "without BWI" to make the plots   more readable. Use "step" instead of "lines" for accuracy. Use   points in all plots to make them more readable. Give different   values of "every" for different types of plots to make them   readable. Use consistent line and point types (circled solid for   soft replenishment, boxed dash for hard replenishment).

--- bwi-client_server/thesis_evaluation_mkcdf.sh
+++ bwi-client_server/thesis_evaluation_mkcdf.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# soft replenishment: lt 1 pt 6
+# hard replenishment: lt 2 pt 4
+
+every_BWI_no_3=30
+every_BWI_no=120
+every_BWI_yes_47=30
+every_BWI_yes=80
+every_BWI_no_hrt=20
+every_BWI_yes_hrt=30
+
 for ((i = 1; i <= 8; i++)); do
 
     if [ $i -eq 3 ]; then
@@ -8,7 +18,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lw 2 title "Soft replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lt 1 lw 2 title "", "" every '$every_BWI_no_3' w points pt 6 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 1 pt 6 ps 2 lw 2 title "Soft replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_no_soft.eps";
 replot'
@@ -17,7 +27,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lw 2 title "Hard replenishment";
+plot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lt 2 lw 2 title "", "" every '$every_BWI_no_3' w points pt 4 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 2 pt 4 ps 2 lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_no_hard.eps";
 replot'
@@ -27,8 +37,8 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lw 2 title "Soft replenishment";
-replot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lw 2 title "Hard replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_no.txt" w steps lt 1 lw 2 title "", "" every '$every_BWI_no' w points pt 6 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 1 pt 6 ps 2 lw 2 title "Soft replenishment";
+replot "thesis_evaluation_hard_'$i'_BWI_no.txt" w steps lt 2 lw 2 title "", "" every '$every_BWI_no' w points pt 4 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 2 pt 4 ps 2 lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_no.eps";
 replot'
@@ -40,7 +50,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lw 2 title "Soft replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lt 1 lw 2 title "", "" every '$every_BWI_yes_47' w points pt 6 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 1 pt 6 ps 2 lw 2 title "Soft replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_yes_soft.eps";
 replot'
@@ -49,7 +59,7 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lw 2 title "Hard replenishment";
+plot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lt 2 lw 2 title "", "" every '$every_BWI_yes_47' w points pt 4 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 2 pt 4 ps 2 lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_yes_hard.eps";
 replot'
@@ -59,8 +69,8 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lw 2 title "Soft replenishment";
-replot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lw 2 title "Hard replenishment";
+plot "thesis_evaluation_soft_'$i'_BWI_yes.txt" w steps lt 1 lw 2 title "", "" every '$every_BWI_yes' w points pt 6 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 1 pt 6 ps 2 lw 2 title "Soft replenishment";
+replot "thesis_evaluation_hard_'$i'_BWI_yes.txt" w steps lt 2 lw 2 title "", "" every '$every_BWI_yes' w points pt 4 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 2 pt 4 ps 2 lw 2 title "Hard replenishment";
 set term postscript eps;
 set output "thesis_evaluation_'$i'_BWI_yes.eps";
 replot'
@@ -72,12 +82,20 @@ set key bottom;
 set term x11;
 set xlabel "t (us)";
 set ylabel "P[f - a <= t]";
-plot "thesis_evaluation_soft_'$i'_BWI_no_hrt.txt" w steps lw 2 title "Soft replenishment in experiment without BWI";
-replot "thesis_evaluation_hard_'$i'_BWI_no_hrt.txt" w steps lw 2 title "Hard replenishment in experiment without BWI";
-replot "thesis_evaluation_soft_'$i'_BWI_yes_hrt.txt" every 20 w linespoints pt 2 lt 1 lw 2 title "Soft replenishment in experiment with BWI";
-replot "thesis_evaluation_hard_'$i'_BWI_yes_hrt.txt" every 20 w linespoints pt 6 lt 1 lw 2 title "Hard replenishment in experiment with BWI";
+plot "thesis_evaluation_soft_'$i'_BWI_no_hrt.txt" w steps lt 1 lw 2 title "", "" every '$every_BWI_no_hrt' w points pt 6 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 1 pt 6 ps 2 lw 2 title "Soft replenishment in experiment without BWI";
+replot "thesis_evaluation_hard_'$i'_BWI_no_hrt.txt" w steps lt 2 lw 2 title "", "" every '$every_BWI_no_hrt' w points pt 4 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 2 pt 4 ps 2 lw 2 title "Hard replenishment in experiment without BWI";
 set term postscript eps;
-set output "thesis_evaluation_'$i'_hrt.eps";
+set output "thesis_evaluation_'$i'_BWI_no_hrt.eps";
+replot'
+	gnuplot -e '
+set key bottom;
+set term x11;
+set xlabel "t (us)";
+set ylabel "P[f - a <= t]";
+plot "thesis_evaluation_soft_'$i'_BWI_yes_hrt.txt" w steps lt 1 lw 2 title "", "" every '$every_BWI_yes_hrt' w points pt 6 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 1 pt 6 ps 2 lw 2 title "Soft replenishment in experiment with BWI";
+replot "thesis_evaluation_hard_'$i'_BWI_yes_hrt.txt" w steps lt 2 lw 2 title "", "" every '$every_BWI_yes_hrt' w points pt 4 lw 2 ps 2 title "", "" every 1e9 w linespoints lt 2 pt 4 ps 2 lw 2 title "Hard replenishment in experiment with BWI";
+set term postscript eps;
+set output "thesis_evaluation_'$i'_BWI_yes_hrt.eps";
 replot'
     fi
 
